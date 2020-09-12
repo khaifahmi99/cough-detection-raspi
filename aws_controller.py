a92cf85c19440ba9f1d642e1ff346a6da57eb539
khaifahmi99 <khaifahmi99@github.com>
2020-09-12 12:40:45 +1000

    complete updatedb

--- aws_controller.py
+++ aws_controller.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import matplotlib.pyplot as plt
 from botocore.exceptions import ClientError
+from datetime import datetime
 
 FOLDER_NAME = 'img/'
 BUCKET_NAME = 'cough-images'
@@ -15,5 +16,25 @@ def s3_upload(file_name, bucket=BUCKET_NAME):
         response = {'status': 'OK', 'url': f'https://cough-images.s3-ap-southeast-2.amazonaws.com/{file_name}' }
     except ClientError as e:
         response = {'status': 'Error', 'msg': 'Fail to upload file to s3'}
-
     return response
+
+def save_to_db(node_id, inference, table_name='cough-detection'):
+    ts = datetime.now()
+    ts = ts.strftime("%d-%m-%Y %H:%M:%S")
+
+    inference['ts'] = ts
+    inference['confidence'] = int(inference['confidence'])
+
+    table = boto3.resource('dynamodb', region_name='ap-southeast-2').Table(table_name)
+    result = table.update_item(
+        Key={
+            'node-id': node_id,
+        },
+        UpdateExpression="SET status_history = list_append(status_history, :s)",
+        ExpressionAttributeValues={
+            ':s': [inference],
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    return result
+
--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-from aws_controller import s3_upload
+from aws_controller import s3_upload, save_to_db
 from server_controller import predict
 import json
 
-response = s3_upload('test.jpg')
+response = s3_upload('test2.jpg')
 if response['status'] == 'OK':
     img_url = response['url']
     
     # do the fetch to /inference
     response = predict(img_url)
     if response['status'] == 'OK':
-        print(response['label'])
+        if response['label'] == 'Coughing':
+            # save to db
+            result = save_to_db("1", response)
+            print(result)
+        else:
+            print(response['label'])
     else:
         print('An error occured')
