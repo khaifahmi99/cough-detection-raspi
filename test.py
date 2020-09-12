c0fe2f5c389e3ebe862843397b2f679b8c78e9c0
khaifahmi99 <khaifahmi99@github.com>
2020-09-12 14:11:53 +1000

    add capture photo function

--- test.py
+++ test.py
@@ -1,8 +1,34 @@
 from aws_controller import s3_upload, save_to_db
 from server_controller import predict
 import json
+import cv2 as cv
+import time
 
-response = s3_upload('test2.jpg')
+def capture_photo():
+    PIC_TARGET_DIR = 'img/'
+    current_time = int(time.time())
+    cam = cv.VideoCapture(0)
+    if not cam.isOpened():
+        raise Exception("Could not open video device")
+
+    for i in range(30):
+        temp = cam.read()
+    retval, frame = cam.read()
+    if retval is not True:
+        raise ValueError("Can't read frame")
+
+    loc = PIC_TARGET_DIR + '{}.png'.format(current_time)
+
+    cv.imwrite(loc, frame)
+    cam.release()
+    cv.destroyAllWindows()
+    return current_time, frame
+
+img, _ = capture_photo()
+img = str(img) + '.png'
+print(img)
+
+response = s3_upload(img)
 if response['status'] == 'OK':
     img_url = response['url']
     
@@ -17,3 +43,4 @@ if response['status'] == 'OK':
             print(response['label'])
     else:
         print('An error occured')
+
